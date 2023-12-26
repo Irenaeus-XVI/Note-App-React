f4c7fde29b14bdc3bf30a4ee76c0042b68736a71
Ehab-Tarek <ehabtarek166@gmail.com>
2023-12-26 17:12:50 +0200

    minimize Sidebar.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,17 @@
 import style from "./Layout.module.css";
 import Sidebar from "../Sidebar/Sidebar.jsx";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 export default function Layout() {
+
+  const [isMinimized, setIsMinimized] = useState(localStorage.getItem('isMinimized') === 'true')
+  localStorage.setItem('isMinimized', isMinimized)
   return (
     <>
       <div className={`d-flex min-vh-100 align-items-stretch ${style.dark}`}>
-        <div className={`${style.sidebar}`}>
-          <Sidebar />
+        <div className={isMinimized ? `${style['sidebar-mini']}` : `${style.sidebar}`}>
+          <Sidebar isMinimized={isMinimized} setIsMinimized={setIsMinimized} />
         </div>
 
         <div className={`${style.content}`}>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 import { UserContext } from "../../Context/UserContext";
 import { showAddModal } from '../../utils/Note.js'
 import { NoteContext } from "../../Context/NoteContext.jsx";
-export default function Sidebar() {
+export default function Sidebar({ isMinimized, setIsMinimized }) {
 
   const { logOut, token } = useContext(UserContext)
   const { setNotes } = useContext(NoteContext)
@@ -14,30 +14,34 @@ export default function Sidebar() {
       <nav className={`${style.nav} shadow-sm`}>
         <button className="btn btn-main text-capitalize w-100 mb-3" onClick={() => showAddModal({ token, updater: setNotes })}>
           <i className="fa-solid fa-plus me-2"></i>
-          New Note
+          {isMinimized ? '' : ' New Note'}
+
         </button>
         <ul className="list-unstyled">
           <li>
             <NavLink to="/">
               <i className="bi bi-house-heart me-2"></i>
-              Home
+              {isMinimized ? '' : 'Home'}
+
             </NavLink>
           </li>
           <li>
             <NavLink to="/search">
-              <i className="bi bi-search me-2"></i> Search
+              <i className="bi bi-search me-2"></i>
+              {isMinimized ? '' : 'Search'}
+
             </NavLink>
           </li>
           <li>
             <span className="pointer" onClick={logOut}>
               <i className="bi bi-box-arrow-left me-2" ></i>
-              Log Out
+              {isMinimized ? '' : 'Log Out'}
             </span>
           </li>
           <li></li>
         </ul>
-        <div className={`${style.change} shadow pointer`}>
-          <i className={`fa-solid fa-chevron-right `}></i>
+        <div className={`${style.change} shadow pointer`} onClick={() => setIsMinimized(!isMinimized)}>
+          <i className={isMinimized ? `fa-solid fa-chevron-right ` : `fa-solid fa-chevron-left `} ></i>
         </div>
       </nav>
     </>
